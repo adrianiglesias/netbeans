3930bc97cee0c4dc6aec05b9770c26136952e357
adrianiglesias <adrian.echlos@gmail.com>
2015-10-05 19:05:52 +0200

    first commit

---
+++ git/hello_world.java
@@ -0,0 +1,20 @@
+// Definició del package
+package cat.iesjoaquimmir.myapp.views;
+
+// Declaració d'importacions de classes
+import java.util.Random;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+import static java.lang.System.out;
+import static java.lang.System.gc;
+import static java.lang.System.exit;
+// Declaració de la classe
+public class Application {
+
+	public static void main(String[] args) {
+		out.println("Hello World from Java!");
+	
+	}
+}
